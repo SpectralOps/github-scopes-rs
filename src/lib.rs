90f597cb6a06c03e85048e5aeb82b32293b34fb1
Elad Kaplan <kaplan.elad@gmail.com>
2022-05-23 11:07:08 +0300

    change readme.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! `github-scopes-rs` Discover GitHub token scope permission and return you an easy interface for checking token permission before querying GitHub.
 //!
-//! In many cases you trying to do any action to GitHub but you getting unclear permissions error. This project allows you to
-//! get which permission your token has before called GitHub, and if you don't have the right permissions you can tell the user the exec permission the the user needs.
+//! In many cases, you try to do actions to GitHub, but you get unclear permissions errors. This project allows you to get which permission your token has before,
+//! called GitHub, and if you don’t have the right permissions, you can tell the user the exact permission the user needs.
 //!
 //! ## How it works
 //! We called Github api with the given token and get which permissions scope the token has in order the access to the API. Then, the permissions are being converted to a simple object that you can work with.
